10189c5685cd4208050ff506eb38f8fd2dbfff65
Kiran Pandranki <Kiran02468@gmail.com>
2024-01-19 22:28:53 +0000

    Adds code to logic.js to loop through questions array and to check the right answer and deduct the time off for wrong answer

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -11,3 +11,44 @@ var feedbackElement = document.querySelector("#feedback");
 var currentQuestionIndex = 0;
 var timeRemaining = questions.length * 15;
 var timerId;
+
+// Looping through the array of questions and answers to create a list of buttons
+function displayQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionTitleElement = document.getElementById("question-title");
+    questionTitleElement.textContent = currentQuestion.question;
+    choicesContainer.innerHTML = "";
+    currentQuestion.choices.forEach(function (option, i) {
+      var optionButton = document.createElement("button");
+      optionButton.setAttribute("value", option);
+      optionButton.textContent = i + 1 + ". " + option;
+      optionButton.addEventListener("click", handleQuestionClick);
+      choicesContainer.appendChild(optionButton);
+    });
+  }
+  
+  // Checking for the right answer and deducting time off for the wrong answer
+  function handleQuestionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      timeRemaining -= 10;
+      if (timeRemaining < 0) {
+        timeRemaining = 0;
+      }
+      timerElement.textContent = timeRemaining;
+      feedbackElement.textContent = `Incorrect! The correct answer was ${questions[currentQuestionIndex].answer}.`;
+      feedbackElement.style.color = "red";
+    } else {
+      feedbackElement.textContent = "Correct!";
+      feedbackElement.style.color = "green";
+    }
+    feedbackElement.setAttribute("class", "feedback");
+    setTimeout(function () {
+      feedbackElement.setAttribute("class", "feedback hide");
+    }, 2000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+      endQuiz();
+    } else {
+      displayQuestion();
+    }
+  }
