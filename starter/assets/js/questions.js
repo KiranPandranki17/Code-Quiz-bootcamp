0821ffe2004b0aba928b09a176a3d4e32a4b8403
Kiran Pandranki <Kiran02468@gmail.com>
2024-01-19 22:21:52 +0000

    Adds questions.js to the repo

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,43 @@
+var questions = [
+    {
+      question: "What is the result of the following expression: 3 + '3'?",
+      choices: ["6", "'33'", "33", "undefined"],
+      answer: "'33'",
+    },
+    {
+      question: "What does the 'typeof' operator in JavaScript return?",
+      choices: ["Number", "String", "Object", "Undefined"],
+      answer: "String",
+    },
+    {
+      question: "How do you declare a variable in JavaScript?",
+      choices: ["var", "let", "const", "all of the above"],
+      answer: "all of the above",
+    },
+    {
+      question: "What is the purpose of 'localStorage' in JavaScript?",
+      choices: [
+        "To store data permanently on the server",
+        "To store data temporarily on the client-side",
+        "To store data in the browser's cookies",
+        "To execute server-side scripts",
+      ],
+      answer: "To store data temporarily on the client-side",
+    },
+    {
+      question: "Which method is used to remove the last element from an array in JavaScript?",
+      choices: ["pop()", "splice()", "shift()", "unshift()"],
+      answer: "pop()",
+    },
+    {
+      question: "What is the role of 'setTimeout' in JavaScript?",
+      choices: [
+        "To create a delay before executing a function",
+        "To set an interval for executing a function repeatedly",
+        "To pause the execution of a function",
+        "To check if a variable is defined",
+      ],
+      answer: "To create a delay before executing a function",
+    },
+  ];
+  
